fd5b8dba43fdc2c783c3c2891cff5c51c8fcb768
qweikfyr <szymonalaszyszka@gmail.com>
2022-05-18 10:43:25 +0200

    general update

---
+++ tailwind.config.js
@@ -0,0 +1,18 @@
+module.exports = {
+  content: ['./src/**/*.{html,js}'],
+  theme: {
+    colors: {
+      'lgray' : '#585755',
+      'bgmain' : '#F4EDED',
+      'lblue' : '#4381C1',
+      'ngray' : '#3C3744',
+      'neon' : '#F2FF49',
+      'bloody' : '#ED6A5A',
+  },
+  fontFamily:{
+    inter: ['Inter', 'sans-serif'],
+  },
+    extend: {},
+  },
+  plugins: [],
+}
